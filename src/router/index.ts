a52d99ab052f616f35a5f6a2e680ecdc8ba20009
Bandidkw <bandid.kanwong@gmail.com>
2025-12-16 09:40:03 +0700

    Final configuration: Set base path /E-Commerce-Website/ and use Hash Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
 const routes = [
@@ -101,7 +101,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(_to, _from, savedPosition) {
     if (savedPosition) {
